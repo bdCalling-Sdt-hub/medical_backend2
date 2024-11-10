c0981af2f8aa62621db614c308871682a1aa8a9c
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-11-10 12:53:37 +0600

    s

--- src/Controller/ReviewController.js
+++ src/Controller/ReviewController.js
@@ -5,14 +5,61 @@ const FormateRequiredFieldMessage = require("../utils/FormateRequiredFieldMessag
 const Queries = require("../utils/Queries");
 
 // Create Review 
+// const CreateReview = async (req, res) => {
+//     try {
+//         const { id } = req.user
+//         const { receiver, rating, comment, appointmentId } = req.body
+//         const ExistingAppointment = await Appointment.findOne({ doctorId: receiver, review: false, userId: id })
+//         if (!ExistingAppointment) {
+//             return res.send({ success: false, message: 'You have already reviewed this doctor' })
+//         }
+//         const [doctor, appointment, review] = await Promise.all([
+//             Doctor.findByIdAndUpdate(
+//                 receiver,
+//                 [
+//                     {
+//                         $set: {
+//                             rating: {
+//                                 $divide: [
+//                                     { $multiply: ["$rating", "$total_rated"] },
+//                                     { $add: ["$total_rated", 1] }
+//                                 ]
+//                             },
+//                             total_rated: { $add: ["$total_rated", 1] }
+//                         }
+//                     }
+//                 ],
+//             ),
+//             // Doctor.findByIdAndUpdate(receiver, { $inc: { rating: rating, total_rated: 1 } }),
+//             Appointment.findByIdAndUpdate(appointmentId, { $set: { review: true } }),
+//             Review.create({ sender: id, receiver, rating, comment })
+//         ]);
+//         res.send({ success: true, data: review, message: 'Review Given Successfully' })
+//     } catch (error) {
+//         let duplicateKeys = '';
+//         if (error?.keyValue) {
+//             duplicateKeys = FormateErrorMessage(error);
+//             error.duplicateKeys = duplicateKeys;
+//         }
+//         let requiredField = []
+//         if (error?.errors) {
+//             requiredField = FormateRequiredFieldMessage(error?.errors);
+//             error.requiredField = requiredField;
+//         }
+//         res.status(500).send({ success: false, message: requiredField[0] || duplicateKeys || 'Internal server error', ...error });
+//     }
+// }
+
 const CreateReview = async (req, res) => {
     try {
-        const { id } = req.user
-        const { receiver, rating, comment, appointmentId } = req.body
-        const ExistingAppointment = await Appointment.findOne({ doctorId: receiver, review: false, userId: id })
+        const { id } = req.user;
+        const { receiver, rating, comment, appointmentId } = req.body;
+        
+        const ExistingAppointment = await Appointment.findOne({ doctorId: receiver, review: false, userId: id });
         if (!ExistingAppointment) {
-            return res.send({ success: false, message: 'You have already reviewed this doctor' })
+            return res.send({ success: false, message: 'You have already reviewed this doctor' });
         }
+
         const [doctor, appointment, review] = await Promise.all([
             Doctor.findByIdAndUpdate(
                 receiver,
@@ -21,7 +68,7 @@ const CreateReview = async (req, res) => {
                         $set: {
                             rating: {
                                 $divide: [
-                                    { $multiply: ["$rating", "$total_rated"] },
+                                    { $add: [{ $multiply: ["$rating", "$total_rated"] }, rating] },
                                     { $add: ["$total_rated", 1] }
                                 ]
                             },
@@ -29,26 +76,38 @@ const CreateReview = async (req, res) => {
                         }
                     }
                 ],
+                { new: true }
             ),
-            // Doctor.findByIdAndUpdate(receiver, { $inc: { rating: rating, total_rated: 1 } }),
             Appointment.findByIdAndUpdate(appointmentId, { $set: { review: true } }),
             Review.create({ sender: id, receiver, rating, comment })
         ]);
-        res.send({ success: true, data: review, message: 'Review Given Successfully' })
+
+        res.send({ success: true, data: review, message: 'Review Given Successfully' });
     } catch (error) {
         let duplicateKeys = '';
         if (error?.keyValue) {
             duplicateKeys = FormateErrorMessage(error);
             error.duplicateKeys = duplicateKeys;
         }
-        let requiredField = []
+        
+        let requiredField = [];
         if (error?.errors) {
             requiredField = FormateRequiredFieldMessage(error?.errors);
             error.requiredField = requiredField;
         }
-        res.status(500).send({ success: false, message: requiredField[0] || duplicateKeys || 'Internal server error', ...error });
+
+        res.status(500).send({
+            success: false,
+            message: requiredField[0] || duplicateKeys || 'Internal server error',
+            ...error
+        });
     }
-}
+};
+
+
+
+
+
 // delete review
 const DeleteReview = async (req, res) => {
     try {
